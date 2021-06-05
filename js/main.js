fbbc54d43948eadf1d9e83b2b013ee11685c2424
6nikkyhood9 <6nikkyhood9@gmail.com>
2021-06-05 15:55:48 +0300

    new commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.arrow',
+  },
+  breakpoints: { 
+      540: {
+      slidesPerView: 2,
+    }
+  }
+});
+
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".header");
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("menu-button-active");
+  menu.classList.toggle("header-active");
+});
